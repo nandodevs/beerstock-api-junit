d93d3595635c40ffae30881da3ab7bcc53f5db47
Sisnando Junior <nando.devs@gmail.com>
2021-11-25 22:24:02 -0300

    Create Entity and Enum

---
+++ src/main/java/com/nandodevs/beerstockapi/entity/Beer.java
@@ -0,0 +1,39 @@
+package com.nandodevs.beerstockapi.entity;
+
+
+import com.nandodevs.beerstockapi.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BeerType type;
+
+}
---
+++ src/main/java/com/nandodevs/beerstockapi/enums/BeerType.java
@@ -0,0 +1,21 @@
+package com.nandodevs.beerstockapi.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+    LAGER("Lager"),
+    MALZBIER("Malzbier"),
+    WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("Ipa"),
+    STOUT("Stout");
+
+    private final String description;
+
+}
