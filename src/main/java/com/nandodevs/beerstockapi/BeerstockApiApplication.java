fa0d7d88b22ecfc46495b10f5dfc0501fbf873ae
Sisnando Junior <nando.devs@gmail.com>
2021-11-25 22:05:13 -0300

    Start project

---
+++ src/main/java/com/nandodevs/beerstockapi/BeerstockApiApplication.java
@@ -0,0 +1,13 @@
+package com.nandodevs.beerstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerstockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerstockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandodevs/beerstockapi/BeerstockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandodevs.beerstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
